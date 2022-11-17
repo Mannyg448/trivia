8faed60445a78e8cb467b1ce0a1a3a0fb4a8143f
Mannyg448 <manny.g448@hotmail.com>
2022-11-16 18:31:19 -0600

    main page layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,22 @@
 <script setup></script>
 
 <template>
-  <RouterView />
+  <main>
+    <div class="card">
+      <RouterView />
+    </div>
+  </main>
+  
 </template>
+
+
+<style lan="postcss" scoped>
+main {
+  @apply flex justify-center items-center h-screen bg-gradient-to-tr from-red-400 via-gray-300 to-blue-500;
+  & .card {
+    @apply flex h-5/6 min-h-fit w-11/12 flex-col items-center gap-16 overflow-auto rounded-lg bg-white p-8 shadow-lg;
+  }
+}
+</style>
+
+
