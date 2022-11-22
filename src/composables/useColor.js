29b1275d782200f5e637813ac57dd32fd69bb90b
Mannyg448 <manny.g448@hotmail.com>
2022-11-22 00:47:01 -0600

    updating color scheme

--- src/composables/useColor.js
+++ src/composables/useColor.js
@@ -1,5 +1,5 @@
 const useColor = () => {
-    const colors = ['bg-green-500', 'bg-red-500', 'bg-blue-500', 'bg-yellow-500']
+    const colors = ['bg-blue-500', 'bg-red-500', 'bg-slate-500', 'bg-purple-500']
     const getColor = (index) => colors[index % colors.length]
     return { getColor }
   }
